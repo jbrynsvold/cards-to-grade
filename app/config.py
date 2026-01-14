42de5b3bf7e10dc27a0491ce529a3cf2a618708b
jbrynsvold <jbrynsvold@gmail.com>
2026-01-14 15:20:04 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -7,14 +7,19 @@ load_dotenv()
 GOOGLE_SHEET_CARDS_NAME = os.getenv("GOOGLE_SHEET_CARDS_NAME")
 GOOGLE_SERVICE_ACCOUNT_JSON = os.getenv("GOOGLE_SERVICE_ACCOUNT_JSON")
 
-# eBay
+# eBay API
 EBAY_APP_ID = os.getenv("EBAY_APP_ID")
+EBAY_CERT_ID = os.getenv("EBAY_CERT_ID")
+EBAY_DEV_ID = os.getenv("EBAY_DEV_ID")
+EBAY_TOKEN = os.getenv("EBAY_TOKEN")  # This is refreshed automatically in ebay.py
 EBAY_SITE_ID = "0"  # US
-EBAY_CONDITION_NEW = 1000  # Example: for cards
 
 # Discord
 DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
-# Bot settings
+# Bot
 CONCURRENT_EBAY_REQUESTS = int(os.getenv("CONCURRENT_EBAY_REQUESTS", 10))
 PSA10_PROFIT_THRESHOLD = float(os.getenv("PSA10_PROFIT_THRESHOLD", 100))  # in %
+
+# Logging
+LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
